9877b839f7a7e416b2b72b2ce3170155a186190f
Dan Hannigan <danjhannigan@gmail.com>
2018-03-29 15:43:37 -0600

    Bump to analytics

--- src/analytics.js
+++ src/analytics.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
+import router from './router';
 import VueAnalytics from 'vue-analytics'
 
 Vue.use(VueAnalytics, {
-  id: 'UA-30177848-7'
+  id: 'UA-30177848-7', 
+  router,
 })
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import analytics from './analytics';
+import VueAnalytics from './analytics';
 
 Vue.config.productionTip = false;
 
