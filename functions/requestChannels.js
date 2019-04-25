2f25a22bed2a73092198584677f6922226893e23
Dan Hannigan <danjhannigan@gmail.com>
2019-04-25 17:36:31 -0600

    IDK

--- functions/requestChannels.js
+++ functions/requestChannels.js
@@ -4,14 +4,16 @@ console.log('Requesting Channels from Slack');
 const axios = require('axios');
 const fs = require('fs');
 
-const channelLambdaAWS = 'https://fq3vjw2i1k.execute-api.us-east-1.amazonaws.com/dev/channels';
+exports.handler = function (event, context) {
+  const channelLambdaAWS = 'https://fq3vjw2i1k.execute-api.us-east-1.amazonaws.com/dev/channels';
 
-axios({
-  method: 'get',
-  url: channelLambdaAWS,
-}).then((res) => {
-  fs.writeFileSync('../docs/.vuepress/components/data/staticChannels.json', JSON.stringify(res.data), 'UTF-8');
-})
-  .catch((err) => {
-    console.log('Error getting stuff', err);
-  });
+  axios({
+    method: 'get',
+    url: channelLambdaAWS,
+  }).then((res) => {
+    fs.writeFileSync('../docs/.vuepress/components/data/staticChannels.json', JSON.stringify(res.data), 'UTF-8');
+  })
+    .catch((err) => {
+      console.log('Error getting stuff', err);
+    });
+}
