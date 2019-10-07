4ba4a9e8b2813eb331287192efcfd9810273e968
Dan Hannigan <danjhannigan@gmail.com>
2019-10-06 21:28:22 -0600

    Feature/big meetups list (#58)
    * Getting a big ol' return on the meetups

--- static-data/getMeetups.js
+++ static-data/getMeetups.js
@@ -1,25 +1,27 @@
 const axios = require("axios");
 const fs = require("fs");
 const chalk = require("chalk");
-const groups = require("./data-sources/meetups.json");
+const groups = require("./staticMeetupUrls.json");
 
 const meetupFile = "./static-data/staticMeetups.json";
 
 console.log(chalk.cyan("Getting meetups from Meetup"));
 
 async function fetchMeetupEvents() {
-  let groupNames = groups.map(group => group.replace(/-/g, ""));
   // eslint-disable-next-line arrow-body-style
   const groupUrls = groups.map(group => {
-    return axios.get(`https://api.meetup.com/${group}/events?access_token=${process.env.MEETUP_API}&page=3`);
+    return axios.get(`https://api.meetup.com/${group}/events?page=3`);
   });
-  return (groupNames = await Promise.all(groupUrls));
+  return await Promise.all(groupUrls.map(p => p.catch(e => e)));
 }
 
 const sourceMeetups = async () => {
   const groupResponse = await fetchMeetupEvents(groups);
-  const responseData = groupResponse.map(res => res.data);
+  const validResults = groupResponse.filter(result => !(result instanceof Error));
+  const responseData = validResults.map(res => res.data);
   const mergedData = [].concat(...responseData);
+
   fs.writeFileSync(meetupFile, JSON.stringify(mergedData), "UTF-8");
+  console.log(chalk.green(`Success, returned ${mergedData.length} meetups`));
 };
 sourceMeetups();
