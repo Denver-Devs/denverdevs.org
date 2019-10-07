ce192b12eea02b6be6d5e3d9e42e854e0e9f2291
Dan Hannigan <danjhannigan@gmail.com>
2019-10-06 21:23:15 -0600

    Quick tweak, good to go

--- static-data/getMeetups.js
+++ static-data/getMeetups.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const fs = require("fs");
 const chalk = require("chalk");
-const groups = require("./data-sources/staticMeetupUrls.json");
+const groups = require("./staticMeetupUrls.json");
 
 const meetupFile = "./static-data/staticMeetups.json";
 
