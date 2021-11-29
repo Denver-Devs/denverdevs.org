9b44704804004b6b2051428a3257cc62a0bbd9dc
Dan Hannigan <danjhannigan@gmail.com>
2021-11-29 11:43:44 -0700

    Updating site for new messaging (#76)
    * Updating site for new messaging
    
    * Oops unescaped chars
    
    * Updating invite link to vanity URL
    
    * Cleaning up some components
    
    * Gradient adjustments for contrast
    
    * Whoops, one more bump

--- components/Header.jsx
+++ components/Header.jsx
@@ -83,7 +83,7 @@ export default function WithSubnavigation() {
                   <Square
                     size="28px"
                     bg="#2756A5"
-                    bgGradient="linear(to-br, #2756A5, #CC6B61)"
+                    bgGradient="linear(to-br, #2756A5, #7C1D22)"
                     borderRadius="md"
                     mr="2"
                   >
@@ -371,23 +371,19 @@ const NAV_ITEMS = [
   //     },
   //   ],
   // },
-  // {
-  //   label: "Rules & Resources",
-  //   children: [
-  //     {
-  //       label: "General rules",
-  //       href: "/browse-jobs",
-  //     },
-  //     {
-  //       label: "Channel Rules",
-  //       href: "/channel-rules",
-  //     },
-  //     {
-  //       label: "Code of Conduct",
-  //       href: "/browse-jobs",
-  //     },
-  //   ],
-  // },
+  {
+    label: "Rules & Resources",
+    children: [
+      {
+        label: "Discord Rules",
+        href: "/resources/rules",
+      },
+      {
+        label: "Code of Conduct",
+        href: "/resources/code-of-conduct",
+      },
+    ],
+  },
   // {
   //   label: "About",
   //   children: [
