336c6cb000508eb7365fdcd122245178f9ff18a9
Dan Hannigan <danjhannigan@gmail.com>
2021-11-30 19:44:29 -0700

    Updating GA tag, adding low level tracking (#77)

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,23 @@
 import Layout from "@/components/Layout";
+import * as ga from "@/lib/ga";
 import theme from "@/styles/theme";
 import { ChakraProvider } from "@chakra-ui/react";
 // import "@fontsource/comfortaa";
 import "@fontsource/comfortaa/variable.css";
 import "@fontsource/inter/variable.css";
-// import "@fontsource/work-sans";
+import React, { useEffect } from "react";
 
 export default function App({ Component, pageProps, router }) {
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <ChakraProvider theme={theme}>
       <Layout route={router.route}>
--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,22 @@ export default class CustomDocument extends Document {
       <Html>
         <Head>
           <meta charSet="utf-8" />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body style={{ minHeight: "calc(100vh - 60px)" }}>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
