bb0005edb7cc11bdf17450c537fb104982286088
Dan Hannigan <hi@dan-h.dev>
2021-12-28 21:38:12 -0700

    Cleaning up airtable references

--- utils/helpers/airtable.js
+++
@@ -1,9 +0,0 @@
-var Airtable = require("airtable");
-
-var hiringBase = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_HIRING_BASE_ID);
-var lookingBase = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_LOOKING_BASE_ID);
-
-const hiringTable = hiringBase(process.env.AIRTABLE_HIRING_TABLE_NAME);
-const lookingTable = lookingBase(process.env.AIRTABLE_LOOKING_TABLE_NAME);
-
-module.exports = { hiringTable, lookingTable };
