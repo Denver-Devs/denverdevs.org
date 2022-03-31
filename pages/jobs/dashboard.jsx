e3ed8c787bb604f0fc65585277cb1a6d1f5fedfe
Dan Hannigan <hi@dan-h.dev>
2022-03-30 22:41:21 -0600

    Fixing missing title on Dashboard page

--- pages/jobs/dashboard.jsx
+++ pages/jobs/dashboard.jsx
@@ -1,4 +1,5 @@
 import { Box, Flex, Heading } from "@chakra-ui/react";
+import Head from "next/head";
 import React, { useEffect, useState } from "react";
 
 import Account from "@/components/Account";
@@ -32,42 +33,47 @@ export default function Dashboard() {
   }, [user]);
 
   return (
-    <Box marginTop={["24", "32"]}>
-      {!session ? (
-        <Auth />
-      ) : (
-        <Box
-          marginTop={{ base: "20", xl: "28" }}
-          marginBottom={{ base: "6", xl: "20" }}
-        >
-          <Box my="10">
-            <TextPageHeader text="Your Dashboard" />
-            <Flex
-              flexDirection={{ base: "column", md: "row" }}
-              marginTop="4"
-              mx={{ base: "2", xl: "8" }}
-            >
-              <Box flex="auto" marginRight={{ base: "0", md: "10" }}>
-                <Heading as="h3" my="4" fontSize="xl">
-                  Your job posts
-                </Heading>
-                <JobList jobs={userEntries.data} />
-              </Box>
-              <Box
-                flex="auto"
-                maxWidth={{ base: "100%", lg: "400px" }}
-                marginBottom={{ base: "10", lg: "0" }}
-                marginLeft="8"
+    <>
+      <Head>
+        <title>Job Dashboard | Denver Devs</title>
+      </Head>
+      <Box marginTop={["24", "32"]}>
+        {!session ? (
+          <Auth />
+        ) : (
+          <Box
+            marginTop={{ base: "20", xl: "28" }}
+            marginBottom={{ base: "6", xl: "20" }}
+          >
+            <Box my="10">
+              <TextPageHeader text="Your Dashboard" />
+              <Flex
+                flexDirection={{ base: "column", md: "row" }}
+                marginTop="4"
+                mx={{ base: "2", xl: "8" }}
               >
-                <Heading as="h3" my="4" fontSize="xl">
-                  Your Account
-                </Heading>
-                <Account key={session.user.id} session={session} />
-              </Box>
-            </Flex>
+                <Box flex="auto" marginRight={{ base: "0", md: "10" }}>
+                  <Heading as="h3" my="4" fontSize="xl">
+                    Your job posts
+                  </Heading>
+                  <JobList jobs={userEntries.data} />
+                </Box>
+                <Box
+                  flex="auto"
+                  maxWidth={{ base: "100%", lg: "400px" }}
+                  marginBottom={{ base: "10", lg: "0" }}
+                  marginLeft="8"
+                >
+                  <Heading as="h3" my="4" fontSize="xl">
+                    Your Account
+                  </Heading>
+                  <Account key={session.user.id} session={session} />
+                </Box>
+              </Flex>
+            </Box>
           </Box>
-        </Box>
-      )}{" "}
-    </Box>
+        )}{" "}
+      </Box>
+    </>
   );
 }
