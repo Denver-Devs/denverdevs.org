ed3b148c169181ca929cb3e9f7f7877326e1c6aa
Dan Hannigan <danjhannigan@gmail.com>
2022-05-24 10:18:47 -0600

    Adding debug mode

--- pages/_document.js
+++ pages/_document.js
@@ -24,7 +24,9 @@ export default class CustomDocument extends Document {
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
+              page_path: window.location.pathname,${
+                process.env.NODE_ENV === "development" ? "debug_mode: true" : ""
+              }
             });
           `,
             }}
