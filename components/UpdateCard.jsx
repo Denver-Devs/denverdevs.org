f07186eafff329448fab12dac03a37c1d934cf60
Dan Hannigan <hi@dan-h.dev>
2022-07-25 18:17:25 -0600

    fix: killing the bad lorem ipsusm, doh

--- components/UpdateCard.jsx
+++ components/UpdateCard.jsx
@@ -78,10 +78,7 @@ const UpdateCard = ({ frontmatter, href }) => {
               color: "gray.300",
             }}
           >
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora
-            expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos
-            enim reprehenderit nisi, accusamus delectus nihil quis facere in
-            modi ratione libero!
+            {frontmatter.snippet}
           </chakra.p>
         </Box>
 
