e73665d6a9b010259974daabaf56e5704213d661
Tim Dronkers <tdronkers@gmail.com>
2022-10-03 16:04:45 -0600

    refactoring _app to use DefaultSeo component

---
+++ next-seo-config.js
@@ -0,0 +1,15 @@
+const nextSeoConfig = {
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://denverdevs.org/",
+    site_name: "Denver Devs",
+  },
+  twitter: {
+    handle: "@DenverDevs",
+    site: "@DenverDevs",
+    cardType: "summary_large_image",
+  },
+};
+
+export default nextSeoConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,15 @@ import "@fontsource/inter/variable.css";
 
 import { ChakraProvider } from "@chakra-ui/react";
 import PlausibleProvider from "next-plausible";
+import { DefaultSeo } from "next-seo";
 import React, { useEffect } from "react";
 
 import Layout from "@/components/Layout";
 import { UserStateProvider } from "@/context/UserContext";
 import * as ga from "@/lib/ga";
 import theme from "@/styles/theme";
+
+import nextSeoConfig from "../next-seo-config";
 
 export default function App({ Component, pageProps, router }) {
   useEffect(() => {
@@ -27,6 +30,7 @@ export default function App({ Component, pageProps, router }) {
       <ChakraProvider theme={theme}>
         <UserStateProvider>
           <Layout route={router.route}>
+            <DefaultSeo {...nextSeoConfig} />
             <Component {...pageProps} />
           </Layout>
         </UserStateProvider>
