2586ae5b0d4c17f92f5c8ef3b0a2e7482f5b0a1f
Dan Hannigan <danjhannigan@gmail.com>
2022-12-02 21:18:33 -0700

    Update next-seo-config.js

--- next-seo-config.js
+++ next-seo-config.js
@@ -2,12 +2,12 @@ const nextSeoConfig = {
   charSet: "utf-8",
   titleTemplate: "Denver Devs | %s",
   description:
-    "Denver Devs is an online Discord community for software engineers and tech industry professionals in the Denver, Colorado area.",
+    "Denver Devs is an online Discord community for software engineers and developers in the Denver, Colorado area.",
   canonical: "https://denverdevs.org",
   openGraph: {
     title: "Denver Devs | %s",
     description:
-      "Denver Devs is an online Discord community for software engineers and tech industry professionals in the Denver, Colorado area.",
+      "Denver Devs is an online Discord community for software engineers and developers in the Denver, Colorado area.",
     type: "website",
     locale: "en_US",
     url: "https://denverdevs.org/",
